31015e826fa32a7def8465e5925db0be726aed31
Igor Zhirkov <igorjirkov@gmail.com>
2023-10-03 11:32:40 +0200

    version 0.1

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # spacemacs-tablegen-layer
 A layer that packs a modified version of tablegen-mode from llvm
+
+
+This is a separate project for now because for `spacemacs` community it might be better to
+first publish `tablegen-mode` as a package on ELPA and then wrap it (and
+possibly also MLIR related modes) in a layer.
+
+# Installation
+
+1. Copy `tablegen` to `~/.emacs.d/layers/+lang/` directory.
+2. Add `tablegen` to `dotspacemacs-configuration-layers` in `~/.spacemacs`.
---
+++ tablegen/packages.el
@@ -0,0 +1,33 @@
+;;; packages.el --- tablegen layer packages file for Space Macs.
+;;
+;; Copyright (c) 2012-2022 Sylvain Benner & Contributors
+;;
+;; Author: Jeremy Bi <bixuanxbi@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(defconst tablegen-packages
+  '(
+    ansi-color
+    (tablegen-mode :location (recipe :fetcher local))
+    ))
+
+(defun tablegen/init-tablegen-mode ()
+  (use-package 'tablegen-mode
+    (add-to-list 'auto-mode-alist (cons (purecopy "\\.td\\'")  'tablegen-mode))
+    )
+  )
